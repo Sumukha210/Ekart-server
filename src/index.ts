46cbd8dc00bb98f89b2a0da04d9ac556815e7be1
sumukha kb <113685718+sumukhakb@users.noreply.github.com>
2023-06-05 14:54:39 +0530

    move assets folder to src

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 const app = express();
 const port = 5000;
 dotenv.config();
+
+app.use(express.static(path.join(__dirname, 'assets')));
+
+console.log(path.join(__dirname, 'assets'));
 
 app.get('/', (_, res) => {
   res.send('Hello, World!');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port http://localhost:${port}`);
 });
